2ca43199f2b0d0a2eecb1baca32d1afa75aa2989
TimCixo <70210043+TimCixo@users.noreply.github.com>
2025-06-12 17:42:11 +0300

    Enable CORS

--- backend/src/server.py
+++ backend/src/server.py
@@ -4,10 +4,12 @@ import os
 import socket
 import threading
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import subprocess
 import websockets
 
 app = Flask(__name__)
+CORS(app)
 
 # Location of the React frontend
 FRONTEND_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', 'frontend')
